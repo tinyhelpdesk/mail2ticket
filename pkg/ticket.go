0faa220342a254e60401b87dbc54e726d987155f
Björn Torben Hahn <info@bjoerntorben.de>
2022-01-02 00:57:56 +0100

    feat: create ticket interface

--- pkg/ticket.go
+++ pkg/ticket.go
@@ -1,4 +1,21 @@
 package tinyhelpdesk
 
+import "time"
+
 type Ticket struct {
+	ID            uint64
+	Subject       string
+	Status        string
+	CustomerName  string
+	CustomerEMail string
+	Category      string
+	Description   string
+	CreationDate  time.Time
+	ModifiedDate  time.Time
+	Attachment    string
+	Assignee      string
+}
+
+type Tickets struct {
+	Tickets []Ticket
 }
